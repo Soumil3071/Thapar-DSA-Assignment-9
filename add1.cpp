6b4cf5824d02cbe24d99b9225a5f942798728867
Soumil3071 <soumilbhgt@gmail.com>
2025-11-26 23:58:40 +0530

    Add DFS and component counting in add1.cpp
    Implement Depth First Search to count connected components in a graph.

---
+++ add1.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+#define MAX 100
+
+int visited[MAX] = {0};
+int adj[MAX][MAX] = {0};
+
+void DFS(int node, int V) {
+    visited[node] = 1;
+    for (int i = 0; i < V; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            DFS(i, V);
+        }
+    }
+}
+
+int countComponents(int V) {
+    int count = 0;
+    for (int i = 0; i < V; i++) {
+        if (!visited[i]) {
+            DFS(i, V);
+            count++;
+        }
+    }
+    return count;
+}
+
+int main() {
+    int V = 5, E = 3;
+    int edges[][2] = {{0,1}, {1,2}, {3,4}};
+    
+    for (int i = 0; i < E; i++) {
+        int u = edges[i][0], v = edges[i][1];
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    
+    cout << "Number of connected components: " << countComponents(V) << endl;
+    return 0;
+}
