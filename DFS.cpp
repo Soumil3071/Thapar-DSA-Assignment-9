a8b1e469ad24d47233ee64c6173f584d0f15b2f8
Soumil3071 <soumilbhgt@gmail.com>
2025-11-26 23:51:03 +0530

    Add DFS implementation in DFS.cpp
    Implement Depth First Search (DFS) algorithm.

---
+++ DFS.cpp
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#define MAX 100
+
+int visited[MAX] = {0};
+int adj[MAX][MAX] = {0};
+
+void DFS(int node, int V) {
+    visited[node] = 1;
+    printf("%d ", node);
+    
+    for (int i = 0; i < V; i++) {
+        if (adj[node][i] == 1 && !visited[i]) {
+            DFS(i, V);
+        }
+    }
+}
+
+int main() {
+    int V = 5, E = 4;
+    int edges[][2] = {{0,1}, {0,2}, {1,3}, {2,4}};
+    
+    for (int i = 0; i < E; i++) {
+        int u = edges[i][0], v = edges[i][1];
+        adj[u][v] = 1;
+        adj[v][u] = 1;
+    }
+    
+    printf("DFS Traversal: ");
+    DFS(0, V);
+    printf("\n");
+    return 0;
+}
